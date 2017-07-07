2c1fdea01b4958b5c2e3b3519734db91c64a7147
hyunjiiiiiik <yd9603@snu.ac.kr>
2017-07-07 18:42:56 +0900

    hello

--- hi/hi.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-int main(void) {
-	printf("Hello, world!I am studying computer science! So fun!\n");
-	return 0;
-}
--- test/test2/test3/hello.c
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-int main(voidC) {
-    printf("Hello, world! I am studying computer science! So fun\n");
-    return 0;
-}
